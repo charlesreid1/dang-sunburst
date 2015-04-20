4adf4e1971c4a14f6b643ca6af183ca834a0eeb9
Charles Reid <charlesreid1@gmail.com>
2015-04-20 15:15:31 -0700

    fixing up letters page to look cleaner and have some ipsum with the sunburst chart.

--- pelican/angular/letters/plain_sunburst.js
+++ pelican/angular/letters/plain_sunburst.js
@@ -1,6 +1,14 @@
 var ng;
 dir = [];
 
+// hipster jesus api
+// for some hipster ipsum
+var hipster_ipsum = function(N,tagname) {
+    $.getJSON('http://hipsterjesus.com/api?paras='+N+'&html=true', function(data) {
+        $(tagname).html( data.text );
+    });
+};
+
 //////////////////////////////////////
 // Sunburst Directives
 //
@@ -8,10 +16,56 @@ dir = [];
 // that use D3 to draw sunburst charts.
 // 
 
-///////////////////////////////////////////////
-// Plain Ol Sunburst
+ng = a.directive('plainSunburstHead', function($compile) {
 
-ng = a.directive('plainSunburst', function($compile) {
+    function link(scope, element, attr) {
+
+        var el = element[0];
+
+        $(el).empty();
+
+        var h1 = $("<h1 />")
+            .appendTo(el);
+        var b = $("<b />")
+            .text("English Language Letter Frequencies")
+            .appendTo(h1);
+        var descr = $("<p />", {
+            "class" : "lead"
+        }).text("Pickled Pitchfork tattooed, health goth ex Schlitz non tempor fap nostrud chia master cleanse. Slow-carb irony cornhole sustainable taxidermy shabby chic. Voluptate trust fund American Apparel, keffiyeh Brooklyn ullamco before they sold out seitan banh mi mumblecore YOLO XOXO ad. Crucifix excepteur before they sold out heirloom post-ironic labore, pop-up bicycle rights minim cold-pressed et meh sint four dollar toast Schlitz. Forage migas banh mi fixie deserunt irony. Marfa odio anim, kogi do chambray elit XOXO beard single-origin coffee McSweeney's normcore. Dolor Vice aliquip, leggings selfies Godard Marfa.")
+        .appendTo(el);
+
+    }
+    return {
+        restrict: "E",
+        link: link,
+        scope: {}
+    }
+});
+dir.push(ng);
+
+
+
+///////////////////////////////////////////////
+// Plain Sunburst Controls
+
+ng = a.directive('plainSunburstControls', function($compile) {
+    function link(scope, element, attr) { }
+    return {
+        restrict: "E",
+        link: link,
+        scope: {}
+    }
+});
+dir.push(ng);
+
+
+
+
+
+///////////////////////////////////////////////
+// Plain Sunburst Chart
+
+ng = a.directive('plainSunburstChart', function($compile) {
 
     function link(scope, element, attr) {
 
@@ -123,7 +177,9 @@ ng = a.directive('plainSunburst', function($compile) {
             .enter().append("path")
             .attr("d", arc)
             .style("fill", function(d) { 
-                //return color(d.letter);
+                if(d.letter=='root') {
+                    return '#ccc';
+                };
                 return color(Math.round(Math.random()*20));
             });
 
