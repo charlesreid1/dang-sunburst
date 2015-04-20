c6ec6759902381bfa3469eff6eda2713b0a6a4cc
Charles Reid <charlesreid1@gmail.com>
2015-04-20 14:03:18 -0700

    this commit is a version of letters that implements loading a json file, and implementing a promise. the directive then watches the data variable. when the promise is resolved, the directive draws the chart.

--- pelican/angular/letters/modcontrol.js
+++ pelican/angular/letters/modcontrol.js
@@ -4,28 +4,32 @@ var a = angular.module("lettersApp", [], function($interpolateProvider) {
         }
     );
 
-var ldfactory = a.factory('ldfactory', function($http) { 
+var ldfactory = a.factory('ldfactory', function($http, $q) {
 
-    var obj = {payload: null};
+    return {
+        getLetterData : function() {
+            var deferred = $q.defer();
 
-    $http.get('letter_freq.json').success(function(data) {
-        // you can do some processing here
-        obj.payload = data;
-    });    
+            $http.get('letter_freq.json').success(function(data) {
+                deferred.resolve(data);
+            }).error(function(){
+                deferred.reject();
+            });
 
-    return obj;    
+            return deferred.promise;
+        }
+    }
 });
 
 function HelloController($scope,ldfactory) {
     $scope.initialize = function() {
-        var letterData = ldfactory;
-
-        // wait a second for the csv to load
-        setTimeout(function() { 
-            console.log(letterData.payload);
-        }, 1000);
-    };
-};
+        ldfactory.getLetterData().then(
+            function(data) { 
+                $scope.letterData = data; 
+            }
+        );
+    }
+}
 
 // the first few arguments of the list should correspond to the Angular-namespace stuff to feed to HelloController
 var c = a.controller("HelloController", ["$scope", "ldfactory", HelloController]);
--- pelican/angular/letters/sunburst.js
+++ pelican/angular/letters/sunburst.js
@@ -15,77 +15,90 @@ ng = a.directive('plainSunburst', function($compile) {
 
     function link(scope, element, attr) {
 
-        /////////////////////////////////////////
-        // Create chart
-        //
-        // data has not been loaded yet.
-        // start by initializing variables 
-        // that don't depend on the data. 
+        scope.$parent.$watch('letterData',doStuff);
 
-        var el = element[0];
-
-        var margin = {
-            top:    10, 
-            right:  40, 
-            bottom: 10, 
-            left:   40
-        };
-        
-        var width   = 400 - margin.right - margin.left,
-            height  = 400 - margin.top   - margin.bottom;
-        
-        var radius = Math.min(width, height) / 2;
-        
-        var x = d3.scale.linear()
-            .range([0, 2 * Math.PI]);
-        
-        var y = d3.scale.sqrt()
-            .range([0, radius]);
-        
-        var svg = d3.select(el).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
-
-
-        // ---------------
-        // more chart-specific, 
-        // data-independent variables:
-
-        // default sort method: count
-        var partition = d3.layout.partition()
-            .sort(null)
-            .value(function(d) { return 1; });
-
-        var arc = d3.svg.arc()
-            .startAngle( function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-            .endAngle(   function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-            .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-            .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
-
-        // Tween from zero 
-        function arcTweenZero(a, i) {
-            var oi = d3.interpolate({x: 0, dx: 0}, a);
-            function tween(t) {
-                var b = oi(t);
-                a.x0  = 0;//b.x;
-                a.dx0 = 0;//b.dx;
-                return arc(b);
-            }
-            if (i == 0) {
-                // If we are on the first arc, adjust the x domain to match the root node
-                // at the current zoom level. (We only need to do this once.)
-                var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
-                return function(t) {
-                    x.domain(xd(t));
-                    return tween(t);
-                };
-            } else {
-                return tween;
-            }
-        }
+        function doStuff() { 
+            if(!scope.$parent.letterData) { return }
             
+            console.log(scope.$parent.letterData);
+
+        }
+
+        function doMoreStuff() {
+
+            /////////////////////////////////////////
+            // Create chart
+            //
+            // data has not been loaded yet.
+            // start by initializing variables 
+            // that don't depend on the data. 
+
+            var el = element[0];
+
+            var margin = {
+                top:    10, 
+                right:  40, 
+                bottom: 10, 
+                left:   40
+            };
+            
+            var width   = 400 - margin.right - margin.left,
+                height  = 400 - margin.top   - margin.bottom;
+            
+            var radius = Math.min(width, height) / 2;
+            
+            var x = d3.scale.linear()
+                .range([0, 2 * Math.PI]);
+            
+            var y = d3.scale.sqrt()
+                .range([0, radius]);
+            
+            var svg = d3.select(el).append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
+
+
+            // ---------------
+            // more chart-specific, 
+            // data-independent variables:
+
+            // default sort method: count
+            var partition = d3.layout.partition()
+                .sort(null)
+                .value(function(d) { return 1; });
+
+            var arc = d3.svg.arc()
+                .startAngle( function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+                .endAngle(   function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+                .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+                .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+            // Tween from zero 
+            function arcTweenZero(a, i) {
+                var oi = d3.interpolate({x: 0, dx: 0}, a);
+                function tween(t) {
+                    var b = oi(t);
+                    a.x0  = 0;//b.x;
+                    a.dx0 = 0;//b.dx;
+                    return arc(b);
+                }
+                if (i == 0) {
+                    // If we are on the first arc, adjust the x domain to match the root node
+                    // at the current zoom level. (We only need to do this once.)
+                    var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
+                    return function(t) {
+                        x.domain(xd(t));
+                        return tween(t);
+                    };
+                } else {
+                    return tween;
+                }
+            }
+
+        }
+
 //--//--//        /*
 //--//--//        // When switching data: interpolate the arcs in data space.
 //--//--//        function arcTweenData(a, i) {
